289227a2ed15f100d33ebeb676045eff4211f5a6
rahul <shanmugammeera1950@gmail.com>
2026-02-04 20:09:32 +0530

    Use index.jsx pattern

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/approutes.jsx"; // Add .jsx back!
+import AppRoutes from "./routes"; 
 import "./App.css";
 
 function App() {
